aa2a558b882ed601c4c9cc12c747b94de24520f4
ozank <ozankoksal@hotmail.com>
2023-04-19 10:00:52 +0300

    Partially implemented ORM Repo

--- src/repositories/EntityRepository.ts
+++ src/repositories/EntityRepository.ts
@@ -1,11 +1,14 @@
-import { Post } from '../entities/Post'
+import { Post } from '@/lib/orm/entities'
+import { Post as IPost } from '../entities/Post'
 import { Repository } from './RepositoryInterface'
+import { SqlEntityRepository } from '@mikro-orm/sqlite'
+import { Connection, EntityManager, IDatabaseDriver } from '@mikro-orm/core'
 
-export class InMemoryPostRepository implements Repository<Post> {
+export class InMemoryPostRepository implements Repository<IPost> {
   lastId = 0
-  posts: Post[] = []
+  posts: IPost[] = []
 
-  add(partialEntity: Omit<Post, 'id' | 'createdAt'>): Promise<Post> {
+  add(partialEntity: Omit<IPost, 'id' | 'createdAt'>): Promise<IPost> {
     return new Promise((resolve, reject) => {
       const post = {
         ...partialEntity,
@@ -18,9 +21,26 @@ export class InMemoryPostRepository implements Repository<Post> {
     })
   }
 
-  list(offset: number, take: number): Promise<Post[]> {
+  list(offset: number, take: number): Promise<IPost[]> {
     return new Promise((resolve) => {
       resolve(this.posts.slice(offset, offset + take))
     })
+  }
+}
+
+export class ORMPostRepository implements Repository<IPost> {
+  repository: SqlEntityRepository<Post>
+  constructor(private em: EntityManager<IDatabaseDriver<Connection>>) {
+    this.repository = em.getRepository(Post)
+  }
+
+  list(offset: number, take: number): Promise<IPost[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async add(post: Omit<IPost, 'id' | 'createdAt'>) {
+    const postEntity = new Post(post)
+    this.repository.persistAndFlush(postEntity)
+    return post as Post
   }
 }
