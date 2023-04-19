883354d7d998e43a2f6f0c167ff56481b580078d
ozank <ozankoksal@hotmail.com>
2023-04-19 09:45:25 +0300

    Updated jest config to use esm

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,18 @@
 import type { JestConfigWithTsJest } from 'ts-jest'
 
 const jestConfig: JestConfigWithTsJest = {
-  // [...]
-  // Replace `ts-jest` with the preset you want to use
-  // from the above list
-  preset: 'ts-jest',
+  preset: 'ts-jest/presets/default-esm',
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1'
+  },
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.m?[tj]sx?$': [
+      'ts-jest',
+      {
+        useESM: true
+      }
+    ]
   }
 }
 
