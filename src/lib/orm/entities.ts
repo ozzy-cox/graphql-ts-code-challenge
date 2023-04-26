154eaa6a650707d9b92a8b63da88a22fbed4851b
ozank <ozankoksal@hotmail.com>
2023-04-26 09:27:40 +0300

    Moved createdAt out of node object

--- src/lib/orm/entities.ts
+++ src/lib/orm/entities.ts
@@ -1,12 +1,10 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core'
+import { Collection, Entity, ManyToOne, OneToMany, PrimaryKey, Property } from '@mikro-orm/core'
 import { Post as IPost, Node as INode } from '../../entities/Post'
 
+// TODO should be renamed to base entity
 export abstract class Node implements INode {
   @PrimaryKey({ autoincrement: true })
   id!: number
-
-  @Property()
-  createdAt = new Date()
 }
 
 @Entity()
@@ -14,8 +12,18 @@ export class Post extends Node implements IPost {
   @Property()
   content!: string
 
+  @ManyToOne(() => Post)
+  parent?: Post
+
+  @Property()
+  createdAt = new Date()
+
+  @OneToMany(() => Post, (post) => post.parent)
+  comments? = new Collection<Post>(this)
+
   constructor(post: Omit<IPost, 'id' | 'createdAt'>) {
     super()
     this.content = post.content
+    this.parent = post.parent
   }
 }
