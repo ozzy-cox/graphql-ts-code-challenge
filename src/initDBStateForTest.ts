763e8033e24ecef5d2153f42ecf57c8e895108f6
ozank <ozankoksal@hotmail.com>
2023-04-26 09:25:21 +0300

    Moved db reset function out of initialization

--- src/initDBStateForTest.ts
+++ src/initDBStateForTest.ts
@@ -1,13 +1,15 @@
 import { PostController } from '@/entities/Post'
-import { Post } from '@/lib/orm/entities'
 import { getOrm } from '@/lib/orm/orm'
 import config from '@/mikro-orm-test.config'
 import { ORMPostRepository } from '@/repositories/EntityRepository'
 
+export const wipeDb = async () => {
+  const orm = await getOrm(config)
+  await orm.getSchemaGenerator().refreshDatabase()
+}
+
 export const initDBStateForTest = async () => {
   const orm = await getOrm(config)
-  await orm.schema.refreshDatabase()
-
   const postController = new PostController(new ORMPostRepository(orm.em.fork()))
 
   postController.createPost('Have a nice day!')
