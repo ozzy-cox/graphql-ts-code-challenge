d2b815dd4e011c70a4d05fdf5fade5bcaf90616b
ozank <ozankoksal@hotmail.com>
2023-05-01 22:44:57 +0300

    Added post resolver test

---
+++ tests/listPostsResolver.test.ts
@@ -0,0 +1,40 @@
+import { Post } from '@/entities/Post'
+import { resolvers } from '@/lib/gql/resolvers/post'
+import { PostRepository } from '@/repositories/PostRepository'
+import { mockContext } from '@/repositories/mock/mockContext'
+import { every, range } from 'lodash-es'
+
+describe('listing posts using the resolver', () => {
+  const posts: Post[] = []
+  const context = mockContext()
+  beforeAll(async () => {
+    await Promise.all(
+      range(10).map(async (idx: number) => {
+        const post = await context.postController.createPost(`Post content ${idx}`)
+        if (post) posts.push(post)
+      })
+    )
+  })
+
+  test('should list posts', async () => {
+    const limit = 4
+    const offset = 2
+    const args = {
+      offset,
+      limit
+    }
+
+    const postsResponse = await resolvers.Query.posts(undefined, args, context)
+
+    expect(postsResponse.length).toEqual(limit)
+    expect(
+      every(postsResponse, (post) => {
+        return (
+          posts.find(({ id }: { id: number }) => {
+            return id === post.id
+          })?.content === post?.content
+        )
+      })
+    ).toBeTruthy()
+  })
+})
