a311acf8ddbd33f1672ab478b4ad1f04c0dd8c8b
ozank <ozankoksal@hotmail.com>
2023-05-11 22:45:40 +0300

    Added service map and service interface for getting generic entity

--- src/context.ts
+++ src/context.ts
@@ -3,10 +3,13 @@ import { PostService } from '@/post/services/PostService'
 import { ReactionRepository } from '@/reaction/infra/orm/repositories/ReactionRepository'
 import { ORM } from './orm'
 import { ReactionService } from './reaction/services/ReactionService'
+import { INodeService } from './shared/services/INodeService'
+import { IPost } from './post/entities/IPost'
 
 export type Context = {
   postService: PostService
   reactionService: ReactionService
+  serviceMap: Record<string, INodeService<IPost>>
 }
 
 export const context = async (): Promise<Context> => {
@@ -14,8 +17,12 @@ export const context = async (): Promise<Context> => {
   const em = orm.em.fork()
   const postService = new PostService(new PostRepository(em))
   const reactionService = new ReactionService(new ReactionRepository(em))
+  const serviceMap = {
+    Post: postService
+  }
   return {
     postService,
-    reactionService
+    reactionService,
+    serviceMap
   }
 }
